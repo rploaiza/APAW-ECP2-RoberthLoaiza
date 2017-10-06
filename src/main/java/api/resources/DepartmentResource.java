36aabf02a99cd63f9baa09a7d85fe3b745bc3e4b
Roberth Loaiza <pauloaiza@hotmail.es>
2017-10-06 02:54:31 +0200

    ACT-#1 : POST /department

--- src/main/java/api/resources/DepartmentResource.java
+++ src/main/java/api/resources/DepartmentResource.java
@@ -4,18 +4,17 @@ import api.controllers.DepartmentController;
 import api.resources.exceptions.DepartmentFieldInvalidException;
 
 public class DepartmentResource {
-	
+
 	public static final String DEPARTMENT = "department";
 
 	public void createDepartment(String departmentTitle, String center) throws DepartmentFieldInvalidException {
 		this.validateField(departmentTitle, center);
+		new DepartmentController().createDepartment(departmentTitle, center);
 	}
 
 	private void validateField(String field, String field2) throws DepartmentFieldInvalidException {
 		if (field == null || field2 == null) {
 			throw new DepartmentFieldInvalidException("Error de" + field + "o de " + field2);
-		} else {
-			new DepartmentController().createDepartment(field, field2);
 		}
 	}
 }
